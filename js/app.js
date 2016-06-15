b453669eb70d58936d925f146a0ea5069752b521
andrewplan <a.plan88@gmail.com>
2016-06-14 20:23:16 -0500

    Added JQuery code to enable Ryu's cool pose when pressing x, fixed mouseenter and mouseleave related bugs.

--- js/app.js
+++ js/app.js
@@ -1,21 +1,65 @@
 $(document).ready(function(){
+    
     $('.ryu').mouseenter(function() {
         $('.ryu-still').hide();
+        $('.ryu-cool').hide();
         $('.ryu-ready').show();
     })
     .mouseleave(function() {
         $('.ryu-ready').hide();
+        $('.ryu-cool').hide();
         $('.ryu-still').show();
     })
     .mousedown(function() {
-        //play hadouken sound
+        playHadouken();
         $('.ryu-ready').hide();
         $('.ryu-throwing').show();
-        $('.hadouken').show();
-        //animate hadouken to the right of screen
+        $('.hadouken').finish().show()
+        .animate(
+            {'left': '1020px'},
+            500,
+            function() {
+                $(this).hide();
+                $(this).css('left', '520px');
+            });
     })
     .mouseup(function() {
         $('.ryu-throwing').hide();
         $('.ryu-ready').show();
     });
+    
+    $('body').keydown(function(event) {
+        if ( event.keyCode == 88 ) {
+            $('.ryu-cool').show(); // code to show cool Ryu pose when hitting x on keyboard
+            $('.ryu-still').hide();
+            $('.ryu-ready').hide();
+            $('.ryu-throwing').hide();
+            $('.hadouken').hide();
+        }; 
+        }).keyup(function() {
+            $('.ryu-cool').hide();
+            $('.ryu-ready').hide();
+            $('.ryu-throwing').hide();
+            $('.hadouken').hide();
+            $('.ryu-still').show();
+        /*}).mouseenter(function() {
+            $('.ryu-still').show
+            $('.ryu-cool').hide();
+            $('.ryu-ready').hide();
+            $('.ryu-throwing').hide();
+            $('.hadouken').hide();
+        }).mouseleave(function() {
+            $('.ryu-still').show
+            $('.ryu-cool').hide();
+            $('.ryu-ready').hide();
+            $('.ryu-throwing').hide();
+            $('.hadouken').hide();
+        })*/
+    });
 });
+
+function playHadouken() {
+    $('#hadouken-sound')[0].volume = 0.5;
+    $('#hadouken-sound')[0].load();
+    $('#hadouken-sound')[0].play();
+}
